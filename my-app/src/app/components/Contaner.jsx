f0a9723e06a2ee613feabf9bcd26d642d42f279b
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-18 11:28:46 +0330

    commit

--- my-app/src/app/components/Contaner.jsx
+++ my-app/src/app/components/Contaner.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Contaner() {
   return (
-    <div className=" flex flex-row h-96 bg-slate-600 ">
-      <div className=" w-2/12">left</div>
-      <div className="bg-gray-500 w-8/12  ">center</div>
+    <div className=" flex flex-row h-96  ">
+      <div className=" w-2/12 bg-green-200">left</div>
+      <div className="bg-gray-50 w-8/12  ">center</div>
       <div className="2/12">left</div>
     </div>
   );
